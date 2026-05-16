74c108687711d2066aef150e19d43d09224a719f
parisalyon69 <jayantuno@gmail.com>
2026-05-16 19:04:36 +0200

    feat: add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Lora, Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 import './globals.css'
 
 const lora = Lora({
@@ -33,7 +34,7 @@ export default function RootLayout({
       lang="en"
       className={`${lora.variable} ${inter.variable} h-full`}
     >
-      <body className="min-h-full flex flex-col antialiased">{children}</body>
+      <body className="min-h-full flex flex-col antialiased">{children}<Analytics /></body>
     </html>
   )
 }
